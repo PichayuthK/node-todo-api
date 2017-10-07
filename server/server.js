602ef3c5883a120252bca8a085c9842d1f61dba6
Peace PC <pichayuthkitti@gmail.com>
2017-10-07 10:28:37 +0700

    add GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose.js');
 var { Todo } = require('./models/todo.js');
@@ -32,7 +33,27 @@ app.get('/todos', (req, res) => {
     });
 });
 
+// GET/todos/123432
+// :{name} URL parameter
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    //res.send(req.params);
+    if (!(ObjectID.isValid(id))) {
+        return res.status(404).send();
+    }
 
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            res.status(404).send();
+        }
+
+        res.send({todo });
+
+    }).catch((e) => {
+        res.status(404).send();
+    });
+
+});
 
 app.listen(3000, () => {
     console.log('server is up on port 3000');
