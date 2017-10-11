ef2027c25ea3ed11c6f205d357dbc451e609c2cb
Peace PC <pichayuthkitti@gmail.com>
2017-10-11 12:38:28 +0700

    add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -4,11 +4,12 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
+const bcrypt = require('bcryptjs');
 
 var { mongoose } = require('./db/mongoose.js');
 var { Todo } = require('./models/todo.js');
 var { User } = require('./models/user.js');
-var {authenticate} = require('./middleware/authenticate.js');
+var { authenticate } = require('./middleware/authenticate.js');
 
 var app = express();
 const port = process.env.PORT;
@@ -124,8 +125,21 @@ app.post('/users', (req, res) => {
 
 });
 
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
 
-app.get('/users/me', authenticate,(req, res) => {
+    User.findByCredentials(body.email, body.password).then((user) => {
+        console.log(user);
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
+
+});
+
+app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 
 });
