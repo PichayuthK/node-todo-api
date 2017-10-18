10a3b0dd437571b9112fceb01d411883b75e1c93
Peace PC <pichayuthkitti@gmail.com>
2017-10-18 16:18:09 +0700

    associate users with todos

--- server/server.js
+++ server/server.js
@@ -16,10 +16,11 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
-    console.log(req.body);
+app.post('/todos', authenticate,(req, res) => {
+   // console.log(req.body);
     var todo = new Todo({
-        text: req.body.text
+        text: req.body.text,
+        _creator: req.user._id
     });
 
     todo.save().then((doc) => {
@@ -29,8 +30,8 @@ app.post('/todos', (req, res) => {
     });
 });
 
-app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
+app.get('/todos', authenticate,(req, res) => {
+    Todo.find({_creator: req.user._id}).then((todos) => {
         res.send({
             todos
         });
@@ -61,14 +62,17 @@ app.get('/todos/:id', (req, res) => {
 
 });
 
-app.delete('/todos/:id', (req, res) => {
+app.delete('/todos/:id',authenticate, (req, res) => {
     var id = req.params.id;
 
     if (!ObjectID.isValid(id)) {
         return res.status(404).send();
     }
 
-    Todo.findByIdAndRemove({ _id: id }).then((todo) => {
+    Todo.findOneAndRemove({ 
+        _id: id,
+        _creator: req.user._id
+    }).then((todo) => {
         if (!todo) {
             return res.status(404).send();
         }
@@ -81,7 +85,7 @@ app.delete('/todos/:id', (req, res) => {
 });
 
 //update
-app.patch('/todos/:id', (req, res) => {
+app.patch('/todos/:id', authenticate, (req, res) => {
     var id = req.params.id;
     var body = _.pick(req.body, ['text', 'completed']);
 
@@ -96,7 +100,10 @@ app.patch('/todos/:id', (req, res) => {
         body.completedAt = null;
     }
 
-    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+    Todo.findOneAndUpdate({
+        _id : id,
+        _creator: req.user._id
+    }, { $set: body }, { new: true }).then((todo) => {
         if (!todo) {
             return res.status(404).send();
         }
